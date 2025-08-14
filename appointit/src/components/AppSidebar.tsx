39282db212746b673415a66e4b9838811072c3a4
Seikixo <150346850+Seikixo@users.noreply.github.com>
2025-08-14 20:20:35 +0800

    chore: add header with logo in sidebar

--- appointit/src/components/AppSidebar.tsx
+++ appointit/src/components/AppSidebar.tsx
@@ -1,4 +1,4 @@
-import { LayoutDashboard, Building2 ,LogOut } from "lucide-react"
+import { LayoutDashboard, Building2, LogOut } from "lucide-react";
 
 import {
   Sidebar,
@@ -7,10 +7,12 @@ import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
+  SidebarMenuBadge,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
 import {
   Dialog,
   DialogTrigger,
@@ -19,8 +21,8 @@ import {
   DialogTitle,
   DialogFooter,
   DialogClose,
-} from "@/components/ui/dialog"
-import { useDispatch } from "react-redux"
+} from "@/components/ui/dialog";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logoutUser } from "@/services/auth";
 import { clearCredentials } from "@/store/authSlice";
@@ -37,76 +39,86 @@ const items = [
     url: "/organization",
     icon: Building2,
   },
-]
+];
 
 export function AppSidebar() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const handleLogout = async () => {
-        try{
-            await logoutUser();
-            dispatch(clearCredentials());
-            navigate("/login");
-        }
-        catch(err){
-            console.error("Logout failed", err);
-        }
+  const handleLogout = async () => {
+    try {
+      await logoutUser();
+      dispatch(clearCredentials());
+      navigate("/login");
+    } catch (err) {
+      console.error("Logout failed", err);
     }
+  };
 
   return (
     <Sidebar collapsible="icon">
-        <SidebarContent>
-            <SidebarGroup>
-                <SidebarGroupLabel>AppointIt</SidebarGroupLabel>
-                <SidebarGroupContent>
-                <SidebarMenu>
-                    {items.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton asChild>
-                        <a href={item.url}>
-                            <item.icon />
-                            <span>{item.title}</span>
-                        </a>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                    ))}    
-                </SidebarMenu>
-                </SidebarGroupContent>
-            </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter>
-            <Dialog>
-                <DialogTrigger asChild>
-                <SidebarMenuButton asChild>
-                    <button type="button" className="flex items-center gap-2 w-full text-left cursor-pointer">
-                    <LogOut className="h-4 w-4" />
-                    <span>Logout</span>
-                    </button>
-                </SidebarMenuButton>
-                </DialogTrigger>
+      <SidebarHeader>
+        <SidebarMenuBadge>
+          <div className="flex flex-row gap-2">
+            <img className="h-7 w-7" src="./AppointIt-Logo.png" alt="" />
+            <h1 className="text-lg">AppointIt</h1>
+          </div>
+        </SidebarMenuBadge>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Main</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <Dialog>
+          <DialogTrigger asChild>
+            <SidebarMenuButton asChild>
+              <button
+                type="button"
+                className="flex items-center gap-2 w-full text-left cursor-pointer"
+              >
+                <LogOut className="h-4 w-4" />
+                <span>Logout</span>
+              </button>
+            </SidebarMenuButton>
+          </DialogTrigger>
 
-                <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Confirm Logout</DialogTitle>
-                </DialogHeader>
-                <p>Are you sure you want to log out?</p>
-                <DialogFooter className="gap-2 sm:justify-end">
-                    <DialogClose asChild>
-                    <button className="px-4 py-2 text-sm bg-gray-200 rounded hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 cursor-pointer">
-                        Cancel
-                    </button>
-                    </DialogClose>
-                    <button
-                    onClick={handleLogout}
-                    className="px-4 py-2 text-sm bg-red-600 text-white rounded hover:bg-red-700 cursor-pointer"
-                    >
-                    Logout
-                    </button>
-                </DialogFooter>
-                </DialogContent>
-            </Dialog>
-        </SidebarFooter>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Confirm Logout</DialogTitle>
+            </DialogHeader>
+            <p>Are you sure you want to log out?</p>
+            <DialogFooter className="gap-2 sm:justify-end">
+              <DialogClose asChild>
+                <button className="px-4 py-2 text-sm bg-gray-200 rounded hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 cursor-pointer">
+                  Cancel
+                </button>
+              </DialogClose>
+              <button
+                onClick={handleLogout}
+                className="px-4 py-2 text-sm bg-red-600 text-white rounded hover:bg-red-700 cursor-pointer"
+              >
+                Logout
+              </button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </SidebarFooter>
     </Sidebar>
-  )
+  );
 }
